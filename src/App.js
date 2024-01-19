0edaf7c7e98d608307c861da68b981f3299eb905
lizziegitau <lizziegitau0505@gmail.com>
2024-01-20 02:36:14 +0300

    error fix

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import MovieCard from './MovieCard';
 import searchIcon from './search.svg';
 
-const API_URL='http://www.omdbapi.com?apikey=7210be85';
+const API_URL='https://www.omdbapi.com?apikey=7210be85';
 
 const App = () => {
     const [searchMovie, setSearchMovie] = useState("");
